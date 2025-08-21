2b3e90fb1283bdb2a0c942e767ed297d9dd7f95e
wer-zen <zenwerden@gmail.com>
2025-08-21 22:58:30 +0200

    hx

---
+++ quickshell/Services/Themes/templates/quickshell-colors.js
@@ -0,0 +1,27 @@
+
+var background = "{{colors.background.default.hex | set_lightness: -4.0 }}"
+var foreground = "{{colors.white.default.hex | set_lightness: +20.0}}"
+var surface = "{{colors.surface.default.hex}}";
+var primary = "{{colors.primary.default.hex}}";
+var secondary = "{{colors.secondary.default.hex}}";
+
+var color0 = "{{colors.shadow.default.hex}}";
+var color1 = "{{colors.red.default.hex}}";
+var color2 = "{{colors.green.default.hex}}";
+var color3 = "{{colors.orange.default.hex}}";
+var color4 = "{{colors.green.default.hex}}";
+var color5 = "{{colors.magenta.default.hex}}";
+var color6 = "{{colors.cyan.default.hex}}";
+var color7 = "{{colors.white.default.hex}}";
+
+var color8 = "{{colors.gray.default.hex}}";
+var color9 = "{{colors.pastel_red.default.hex}}";
+var color10 = "{{colors.pastel_green.default.hex}}";
+var color11 = "{{colors.pastel_orange.default.hex}}";
+var color12 = "{{colors.pastel_blue.default.hex}}";
+var color13 = "{{colors.pastel_magenta.default.hex}}";
+var color14 = "{{colors.pastel_mint_green.default.hex}}";
+var color15 = "{{colors.white.default.hex}}";
+
+
+// ... more colors
